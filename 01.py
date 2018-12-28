4db9c82f2a7c2213c406e65d258df57f1c3f2762
Danicaxuan <danicaxuan@icloud.com>
2018-12-28 16:59:57 +0800

    go on

--- 01.py
+++ 01.py
@@ -1,13 +1,37 @@
 '''
 定义一个学生类，用来形容学生
 '''
-#定义一个空的类
+# 定义一个空的类
 class Student():
     #一个空类，pass代表直接跳过
     #此处pass必须有
     pass
 
-#定义一个对象
+# 定义一个对象
 mingyue = Student()
 
-#在定义一个
+# 在定义一个类，用来描述听python的学生
+class PythonStudent():
+    #用None给不确定的值赋值
+    name = None
+    age = 18
+    course = "Python"
+
+    # 需要注意：
+    # 1. def doHomework 的缩进层级
+    # 2。系统默认有一个self参数
+    def doHomework(self):
+        print("I 在做作业")
+
+        #推荐在函数末尾使用retun语句
+        return
+
+# 实例化一个叫yueyeu的学生，是一个具体的人
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.course)
+print(yueyue.age)
+#注意成员函数的调用没有传递进入参数
+yueyue.doHomework()
+
+
