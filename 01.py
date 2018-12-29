0d6686626ad880110018eb0ab69a741d10b4e470
Danicaxuan <danicaxuan@icloud.com>
2018-12-29 16:53:10 +0800

    goon

--- 01.py
+++ 01.py
@@ -1,18 +1,22 @@
 '''
 定义一个学生类，用来形容学生
 '''
+
+
 # 定义一个空的类
 class Student():
-    #一个空类，pass代表直接跳过
-    #此处pass必须有
+    # 一个空类，pass代表直接跳过
+    # 此处pass必须有
     pass
+
 
 # 定义一个对象
 mingyue = Student()
 
+
 # 在定义一个类，用来描述听python的学生
 class PythonStudent():
-    #用None给不确定的值赋值
+    # 用None给不确定的值赋值
     name = None
     age = 18
     course = "Python"
@@ -23,15 +27,41 @@ class PythonStudent():
     def doHomework(self):
         print("I 在做作业")
 
-        #推荐在函数末尾使用retun语句
+        # 推荐在函数末尾使用retun语句
         return
+
 
 # 实例化一个叫yueyeu的学生，是一个具体的人
 yueyue = PythonStudent()
 print(yueyue.name)
 print(yueyue.course)
 print(yueyue.age)
-#注意成员函数的调用没有传递进入参数
+# 注意成员函数的调用没有传递进入参数
 yueyue.doHomework()
 
 
+class A():
+    name = "dana"
+    age = 18
+
+    def say(self):
+        self.name = "aaaa"
+        self.age = 200
+
+
+# 此时，A称为类实例
+print(A.name)
+print(A.age)
+
+print('*' * 20)
+print(id(A.name))
+print(id(A.age))
+
+print('*' * 20)
+# 此时，a称为对象实例
+a = A()
+
+print(a.name)
+print(a.age)
+print(id(a.name))
+print(id(a.age))
--- 2-opp.md
+++ 2-opp.md
@@ -4,3 +4,19 @@
 - conda list : 显示anaconda的虚拟环境列表
 - conda env list:显示anaconda的虚拟环境列表
 - conda create -n xxx(oop) python=3.6 :创建python版本为3.6的虚拟环境，名称为XXX
+
+
+
+# 访问对象成员
+    - 使用点操作符
+    - 可以通过默认内置成员检查
+        - 对象所有成员检查
+            #dict前后各有两个下划线
+            obj.__dict__
+            
+        - 类所有的成员
+        
+            # dict 前后各有两个下划线
+            class_name.__dict__
+            
+            
